24be884bc824fca53a23842664f1e2f8fa49e251
Meitnerium9 <gronekmarcinn@gmail.com>
2026-05-17 16:15:48 +0200

    fix: Rozłożenie komponentów
    Rozłożenie komponentów na kilka plików

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Koszyk from "./koszyk/Koszyk.jsx";
 import Register from "./register/App.jsx";
 import Creator from "./creator/Creator.jsx";
-import Post_creator from "./post_creator/Post_creator.jsx";
+import PostCreator from "./postCreator/PostCreator.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -16,7 +16,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <Route path="/koszyk" element={<Koszyk />} />
         <Route path="/register" element={<Register />} />
         <Route path="/creator" element={<Creator />} />
-        <Route path="/post_creator" element={<Post_creator />} />
+        <Route path="/postCreator" element={<PostCreator />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
---
+++ frontend/src/postCreator/PostCreator.jsx
@@ -0,0 +1,50 @@
+import "../App.css";
+import { useState, useRef } from "react";
+import "./PostCreator.css";
+
+import PostCreatorTitle from "./components/PostCreatorTitle.jsx";
+import PostCreatorImageUpload from "./components/PostCreatorImageUpload.jsx";
+import PostCreatorDescription from "./components/PostCreatorDescription.jsx";
+import PostCreatorActions from "./components/PostCreatorActions.jsx";
+
+import Navbar from "../components/Navbar.jsx";
+
+function PostCreator() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [image, setImage] = useState(null);
+  const fileInputRef = useRef(null);
+
+  const handleSave = () => {
+    console.log("Zapisywanie posta:", { title, description, image });
+    alert("Post został zapisany!");
+  };
+
+  return (
+    <>
+      <Navbar />
+      <main className="post-creator-page">
+        <div className="post-creator-wrapper">
+          <PostCreatorTitle title={title} setTitle={setTitle} />
+
+          <div className="post-creator-row">
+            <PostCreatorImageUpload
+              image={image}
+              setImage={setImage}
+              fileInputRef={fileInputRef}
+            />
+
+            <PostCreatorDescription
+              description={description}
+              setDescription={setDescription}
+            />
+          </div>
+
+          <PostCreatorActions onSave={handleSave} />
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default PostCreator;
