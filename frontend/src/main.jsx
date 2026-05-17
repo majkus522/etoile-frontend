04002a48293fba4e88ab2365a8a1ca5bfbf1defc
majkus522 <mikohus522@gmail.com>
2026-05-17 13:43:25 +0200

    Fix main page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Koszyk from './koszyk/Koszyk.jsx'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/koszyk" element={<Koszyk />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
-);
+)
