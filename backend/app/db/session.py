e4e6d7ed1d14d813af436eab442be8dd0dca435c
pjanusz538 <pjanusz538@gmail.com>
2026-05-17 12:16:20 +0200

    nothing

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql+psycopg2://etoile_admin:supertajnehaslo123@db:5432/etoile_db"
+DATABASE_URL = "postgresql+psycopg2://etoile_admin:supertajnehaslo123@db:5400/etoile_db"
 
 engine = create_engine(DATABASE_URL)
 
--- backend/init.sh
+++ backend/init.sh
@@ -2,12 +2,10 @@
 
 echo "Waiting for database..."
 
-while ! nc -z db 5432; do
+while ! nc -z postgres_db 5432; do
   sleep 0.5
 done
 
 echo "Database is up!"
-
-python -m app.db.init_db
 
 uvicorn app.main:app --host 0.0.0.0 --port 8000
