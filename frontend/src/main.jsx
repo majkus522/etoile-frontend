a03be6ac46e64ec92eb54f1f08241ddadbe1d861
majkus522 <mikohus522@gmail.com>
2026-05-20 14:35:12 +0200

    Format

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,23 +11,22 @@ import Blog from "./blog/Blog.jsx";
 import { useEffect } from "react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/koszyk" element={<Koszyk />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/creator" element={<Creator />} />
-        <Route path="/postCreator" element={<PostCreator />} />
-        <Route path="/blog" element={<Blog />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>,
+	<React.StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<App />} />
+				<Route path="/koszyk" element={<Koszyk />} />
+				<Route path="/register" element={<Register />} />
+				<Route path="/creator" element={<Creator />} />
+				<Route path="/postCreator" element={<PostCreator />} />
+				<Route path="/blog" element={<Blog />} />
+			</Routes>
+		</BrowserRouter>
+	</React.StrictMode>
 );
 
-export function useTitle(title)
-{
-  useEffect(() => {
-    document.title = title;
-  }, [title]);
+export function useTitle(title) {
+	useEffect(() => {
+		document.title = title;
+	}, [title]);
 }
